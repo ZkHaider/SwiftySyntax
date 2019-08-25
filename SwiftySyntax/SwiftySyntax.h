d33f0d830650c3ea1f8008a8e7c102123aeabee5
Haider Khan <hkhan@datalofts.com>
2019-08-25 11:03:23 -0700

    Initial Project Setup

---
+++ SwiftySyntax/SwiftySyntax.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySyntax.h
+//  SwiftySyntax
+//
+//  Created by Haider Khan on 8/25/19.
+//  Copyright © 2019 zkhaider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftySyntax.
+FOUNDATION_EXPORT double SwiftySyntaxVersionNumber;
+
+//! Project version string for SwiftySyntax.
+FOUNDATION_EXPORT const unsigned char SwiftySyntaxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySyntax/PublicHeader.h>
+
+
